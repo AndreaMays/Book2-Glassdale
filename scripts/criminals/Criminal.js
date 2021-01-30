024197a1a79904d71755db06982beec7efdd3858
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-01-29 18:20:35 -0600

    Addd Alibi files/officer button still sucks

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -4,6 +4,7 @@ export const Criminal = (criminal) => {
     <p class="__name">Name: ${criminal.name}</p>
     <p class="__age">Age: ${criminal.age}</p>
     <p class="__conviction">Conviction: ${criminal.conviction}</p>
+    <p class="__arrestingOfficer">Arresting Officer: ${criminal.arrestingOfficer}</p>
     <p class="__incarceration">Start: ${new Date(criminal.incarceration.start).toLocaleDateString('en-US')}</p>
     <p class="__incarceration">End: ${new Date(criminal.incarceration.end).toLocaleDateString('en-US')}</p>
     </div>
